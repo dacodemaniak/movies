b7c21b55d76afdfd54048a4b9d03bfe7e58b0a44
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-01-03 14:37:10 +0100

    With console in contructor of main

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @name Main
+ * @author Jean-Luc (jla.webprojet@gmail.com) - Jan. 2020
+ * @version 1.0.0
+ *  Entry point of the application
+ */
+class Main {
+    public constructor(){
+        console.log('Hello my Main!');
+    }
+}
+
+// Main app instanciation
+new Main();
